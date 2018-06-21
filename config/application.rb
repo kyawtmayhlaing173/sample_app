12f545a14cbb08ee9440095ec6e1f0602a15808d
kmh <pinky.hlaing173@gmail.com>
2018-06-21 10:07:33 +0630

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -10,7 +11,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
